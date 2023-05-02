4e1d7c306fba22a3c1cb00a481674e43efb4600c
David Silva <davidsilvac131@gmail.com>
2023-05-02 17:11:41 -0500

    Update play button component

--- src/components/PlayButton.jsx
+++ src/components/PlayButton.jsx
@@ -2,9 +2,9 @@ import { Button } from '@nextui-org/react'
 import { useQuestionsStore } from '../store/QuizStore'
 
 const PlayButton = () => {
-  const { fetchQuestions } = useQuestionsStore((state) => state)
+  const fetchQuestions = useQuestionsStore((state) => state.fetchQuestions)
   const handlePlay = async () => {
-    await fetchQuestions()
+    fetchQuestions()
   }
   return (
     <Button onPress={handlePlay} shadow color='gradient'>
