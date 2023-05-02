c8ebce144c0237e7eb02cd80cbad2ed9d68664ab
David Silva <davidsilvac131@gmail.com>
2023-05-02 17:12:49 -0500

    Update questions to be returned

--- src/services/QuizServices.js
+++ src/services/QuizServices.js
@@ -1,8 +1,14 @@
 const URL_API = 'https://opentdb.com/api.php'
 const QUIZ_QUANTITY = 10
 export const getQuiz = async () => {
-  const url = `${URL_API}?amount=${QUIZ_QUANTITY}`
+  const url = `${URL_API}?amount=${QUIZ_QUANTITY}&type=multiple`
   const response = await fetch(url)
   const { results } = await response.json()
-  return results
+  const newResults = results.map(q => (
+    {
+      ...q,
+      correctAnswer: q.correct_answer
+    }
+  ))
+  return newResults
 }
