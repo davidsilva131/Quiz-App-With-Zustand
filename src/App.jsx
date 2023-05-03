bc883436c36440fa4ca69432249530c7409ffecb
David Silva <davidsilvac131@gmail.com>
2023-05-02 20:17:36 -0500

    Update conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,23 @@ import { Container, Text } from '@nextui-org/react'
 import PlayButton from './components/PlayButton'
 import { useQuestionsStore } from './store/QuizStore'
 import Quiz from './components/Quiz'
+import { useQuestionsData } from './hooks/useQuestionsData'
+import Results from './components/Results'
 
 function App () {
   const questions = useQuestionsStore((state) => state.questions)
+  const { unanswered } = useQuestionsData()
   return (
     <main className='main'>
       <Container className='container'>
         <section className='app'>
           <div className='tittle'>
-            <Text h1>App</Text>
+            <Text h1>Quiz App</Text>
           </div>
           <div className='content'>
-            {
-            questions.length === 0 ? <PlayButton /> : <Quiz />
-          }
+            {questions.length === 0 && <PlayButton />}
+            {questions.length > 0 && unanswered > 0 && <Quiz />}
+            {questions.length > 0 && unanswered === 0 && <Results />}
           </div>
         </section>
       </Container>
